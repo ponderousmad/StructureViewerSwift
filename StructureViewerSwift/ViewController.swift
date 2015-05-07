cb4743f27405daf3815d122f5247a4b57a451f66
Adrian Smith <agnomen@gmail.com>
2015-05-06 20:24:49 -0400

    Step 9 - Derive view controller from STSensorControllerDelegate and provide stubs for required methods

--- StructureViewerSwift/ViewController.swift
+++ StructureViewerSwift/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, STSensorControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func sensorDidConnect() {}
+    func sensorDidDisconnect() {}
+    func sensorDidStopStreaming(reason: STSensorControllerDidStopStreamingReason) {}
+    func sensorDidLeaveLowPowerMode() {}
+    func sensorBatteryNeedsCharging() {}
 }
 
