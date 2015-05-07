8a5c18e2592779d78a65eb6cbb3aa7d699cd8fe0
Adrian Smith <agnomen@gmail.com>
2015-05-06 20:10:49 -0400

    Initial Commit

---
+++ StructureViewerSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StructureViewerSwift
+//
+//  Created by Adrian Smith on 2015-05-06.
+//  Copyright (c) 2015 Adrian Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
