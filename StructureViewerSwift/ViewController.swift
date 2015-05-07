3bbe706d6514f05705e93a03ac43dd345c0c6361
Adrian Smith <agnomen@gmail.com>
2015-05-06 20:56:45 -0400

    Add image view to show depth.

--- StructureViewerSwift/ViewController.swift
+++ StructureViewerSwift/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController, STSensorControllerDelegate {
 
+    @IBOutlet weak var depthView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
